d6aebd0a56c17051ca0c080253a9de290150875d
kenshin <kenshin.acs@gmail.com>
2013-08-13 15:15:13 +0800

    dns records table
    Complete dns records layout

--- controllers/dns.go
+++ controllers/dns.go
@@ -1,18 +1,42 @@
 package controllers
 
 import (
+	"fmt"
 	"github.com/astaxie/beego"
+	"github.com/hoisie/redis"
 )
+
+func initialRedis() *redis.Client {
+	db, _ := beego.AppConfig.Int("redisdb")
+	rc := &redis.Client{
+		Addr:     beego.AppConfig.String("redisaddr"),
+		Db:       db,
+		Password: beego.AppConfig.String("redispassword"),
+	}
+	return rc
+}
 
 type DNSController struct {
 	beego.Controller
+	rc *redis.Client
 }
 
 // http basic auth
+// redis connect
 func (c *DNSController) Prepare() {
 	CheckAuth(c.Ctx)
+	c.rc = initialRedis()
 }
 
 func (c *DNSController) Get() {
+	var DNSRecords = make(map[string]string)
+	bindkey := beego.AppConfig.String("bindkey")
+	err := c.rc.Hgetall(bindkey, DNSRecords)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(DNSRecords)
+	c.Data["DNS"] = DNSRecords
+	c.Layout = "layout.html"
 	c.TplNames = "dns.html"
 }
