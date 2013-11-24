af935356ce56e3502f7d67031f2220298a39969f
kenshin <kenshin.acs@gmail.com>
2013-11-24 15:53:57 +0800

    Fix the beego.Context issue caused by  beego upgrade.

--- controllers/auth.go
+++ controllers/auth.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/context"
 	auth "github.com/kenshinx/go-http-auth"
 	"strings"
 )
@@ -17,7 +18,7 @@ func Secret(user, realm string) string {
 	return ""
 }
 
-func CheckAuth(ctx *beego.Context) {
+func CheckAuth(ctx *context.Context) {
 	if beego.AppConfig.String("basic_auth") != "" {
 		a := auth.NewBasicAuthenticator("joke.sina", Secret)
 		if username := a.CheckAuth(ctx.Request); username == "" {
